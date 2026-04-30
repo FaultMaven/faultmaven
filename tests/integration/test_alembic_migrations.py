5bed6cc6ce8cdcb8edc6ceb22f9f6f6f4c638ab1
Sterlan Yu <sterlanyu@gmail.com>
2026-04-30 00:56:58 +0000

    doc: faultmaven product description update

--- tests/integration/test_alembic_migrations.py
+++ tests/integration/test_alembic_migrations.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = Path(__file__).parent.parent.parent
 TEST_DB = str(PROJECT_ROOT / "test_migration.db")
 
 # Current head revision
-HEAD_REVISION = "a1b2c3d4e5f6"  # cases.version column for OCC
+HEAD_REVISION = "b2c3d4e5f6a7"  # uploaded_files.size_bytes constraint relax
 
 
 @pytest.fixture(scope="function")
